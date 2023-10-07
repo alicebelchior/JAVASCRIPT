e04509c15ffa0230974ed005bd32d19d26e8f5d8
alicebelchior <alice.belchior23@gmail.com>
2023-10-06 23:23:14 -0300

    Aula 6 - Entrada de dados
    exemplos

---
+++ aula6/ex1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+nome = prompt("Digite seu nome: ");
+idade = prompt("Digite sua idade: ");
+endereco = prompt("Digite seu endereço: ");
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Endereço: ", endereco);
---
+++ aula6/ex3.js
@@ -0,0 +1,12 @@
+//Convertend tipos
+
+const  prompt = require("prompt-sync")();
+
+x = parseInt(prompt("Digite o número 1: "));
+y = parseInt(prompt("Digite o número 2: "));
+
+//somando duas variávis inteiras
+soma = x + y;
+
+console.log("\nx = ", x, "\ny = ", y);
+console.log("\nsoma = ", soma);
