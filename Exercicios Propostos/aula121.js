45951ceb5b0a91573f4a97ee66525279e92a5a0f
alicebelchior <alice.belchior23@gmail.com>
2023-10-24 15:12:32 -0300

    término do exercicio proposto

--- Exercicios Propostos/aula121.js
+++ Exercicios Propostos/aula121.js
@@ -6,7 +6,7 @@ function calcularIMC (peso, altura) {
 }
 
 //Definir a função para verificar situação
-function verfiicarSitu(codigo) {
+function verificarSitu(imc) {
     let codigo;
     if (imc < 18.5) {
         codigo = 1;
@@ -22,13 +22,27 @@ function verfiicarSitu(codigo) {
 
 //Definir procedimento (é uma espécie de função que nao retorna valor)
 function exibirDescricao(codigo) {
-    if (codigo = 1) {
+    if (codigo == 1) {
         console.log("Você está abaixo do peso ideal!");
-    } else if (codigo = 2) {
+    } else if (codigo == 2) {
         console.log("Parabéns! Você está em seu peso normal!");
-    } else if (codigo = 3) {
+    } else if (codigo == 3) {
         console.log("Você está acima de seu peso (sobrepeso)!");
     } else {
         console.log("Obesidade!");
     }    
+}
+
+const prompt = require("prompt-sync")();
+
+for (i = 1; i <= 5; i++) {
+    console.log("PESSOA", i)
+    let nome = prompt("Entre com o nome: ");
+    let peso = parseFloat(prompt("Entre com o peso (kg): "));
+    let altura = parseFloat(prompt("Entre com a altura(m): "));
+
+    //chamando as funções
+    let imc = calcularIMC(peso, altura);
+    let codigo = verificarSitu(imc);
+    exibirDescricao(codigo);
 }
